574d5fc5d93dd07315bc3496b2596e061a2b8e65
xXmario fuentes Xx <160526473+MarioProg4A@users.noreply.github.com>
2024-06-15 01:07:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var numero01;
+var numero02;
+numero01=parseInt(prompt("ingresa el numero 01"),10);
+numero02=parseInt(prompt("ingresa el numero 02"),10);
+if (numero01>numero02)
+
+{
+  document.write("el numero mayor es: "+ numero01 +"(numero 1 )" );
+}
+else
+{
+  document .write("el numero mayor es: "+ numero02 +"(numero 2 )" );
+
+}
+
